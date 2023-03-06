a651066b5b6ff32d4ddbb041c04a3f635a24f0ae
Artemem <113576492+Artemem@users.noreply.github.com>
2023-03-06 13:58:32 +0300

    Update Func.c

--- src/Lab№1/Func.c
+++ src/Lab№1/Func.c
@@ -11,7 +11,7 @@ struct bicycle
 };
 enum tip
 {
-	MOUNTAIN,
+	MOUNTAIN=0,
 	STUNT,
 	URBAN,
 	HYBRID
@@ -19,10 +19,10 @@ enum tip
 int quantity_of_bicycles()
 {
     int n = 0;
-	printf("Введите кол-во велосипедов:");
+	printf("Р’РІРµРґРёС‚Рµ РєРѕР»-РІРѕ РІРµР»РѕСЃРёРїРµРґРѕРІ:");
     while ((scanf_s("%d", &n)) != 1|| getchar() != '\n')
     {
-        printf("Ошибка.Попробуй снова\nВведите кол-во велосипедов:");
+        printf("РћС€РёР±РєР°.РџРѕРїСЂРѕР±СѓР№ СЃРЅРѕРІР°\nР’РІРµРґРёС‚Рµ РєРѕР»-РІРѕ РІРµР»РѕСЃРёРїРµРґРѕРІ:");
         rewind(stdin);
     }
     return n;
@@ -36,11 +36,11 @@ void enter_mas_structure(int n, struct bicycle* mas)
 {
 	for (int i = 0; i < n; i++)
 	{
-		printf("\nВведите год выпуска %d велосипеда:", i + 1);
+		printf("\nР’РІРµРґРёС‚Рµ РіРѕРґ РІС‹РїСѓСЃРєР° %d РІРµР»РѕСЃРёРїРµРґР°:", i + 1);
 		checker(mas, i, 1);
-		//printf("Введите цвет %d велосипеда:", i + 1);
+		//printf("Р’РІРµРґРёС‚Рµ С†РІРµС‚ %d РІРµР»РѕСЃРёРїРµРґР°:", i + 1);
 		checker(mas, i, 2);
-		printf("Введите код класса %d велосипеда (Горный[1], Трюковой[2], Городской[3], Гибридный[4]):", i + 1);
+		printf("Р’РІРµРґРёС‚Рµ РєРѕРґ РєР»Р°СЃСЃР° %d РІРµР»РѕСЃРёРїРµРґР° (Р“РѕСЂРЅС‹Р№[0], РўСЂСЋРєРѕРІРѕР№[1], Р“РѕСЂРѕРґСЃРєРѕР№[2], Р“РёР±СЂРёРґРЅС‹Р№[3]):", i + 1);
 		checker(mas, i, 3);
 	}
 }
@@ -51,7 +51,7 @@ void checker(struct bicycle* mas, int i, int k)
 	{
 		while ((scanf_s("%d", &mas[i].year)) != 1 || mas[i].year>2024 || mas[i].year < 2010 || getchar() != '\n')
 		{
-			printf("Ошибка. Попробуй снова\nВведите год выпуска %d велосипеда:", i+1);
+			printf("РћС€РёР±РєР°. РџРѕРїСЂРѕР±СѓР№ СЃРЅРѕРІР°\nР’РІРµРґРёС‚Рµ РіРѕРґ РІС‹РїСѓСЃРєР° %d РІРµР»РѕСЃРёРїРµРґР°:", i+1);
 			rewind(stdin);
 		}
 	}
@@ -60,16 +60,16 @@ void checker(struct bicycle* mas, int i, int k)
 		mas[i].colour = (char*)calloc(1024, sizeof(char));
 		do
 		{
-			printf("Введите цвет %d велосипеда:", i + 1);
+			printf("Р’РІРµРґРёС‚Рµ С†РІРµС‚ %d РІРµР»РѕСЃРёРїРµРґР°:", i + 1);
 			rewind(stdin);
 			gets_s(mas[i].colour, 20 * sizeof(char));
 		} while (strlen(mas[i].colour) < 3);
 	}
 	if (k == 3)
 	{
-		while ((scanf_s("%d", &mas[i].class)) < 1 || mas[i].class != 1 && mas[i].class != 2 && mas[i].class != 3 && mas[i].class != 4 || getchar() != '\n')
+		while ((scanf_s("%d", &mas[i].class)) < 1 || mas[i].class != 0 && mas[i].class != 1 && mas[i].class != 2 && mas[i].class != 3 || getchar() != '\n')
 		{
-			printf("Ошибка.Попробуй снова\nВведите код класса %d велосипеда (Горный[1], Трюковой[2], Городской[3], Гибридный[4]):", i + 1);
+			printf("РћС€РёР±РєР°.РџРѕРїСЂРѕР±СѓР№ СЃРЅРѕРІР°\nР’РІРµРґРёС‚Рµ РєРѕРґ РєР»Р°СЃСЃР° %d РІРµР»РѕСЃРёРїРµРґР° (Р“РѕСЂРЅС‹Р№[0], РўСЂСЋРєРѕРІРѕР№[1], Р“РѕСЂРѕРґСЃРєРѕР№[2], Р“РёР±СЂРёРґРЅС‹Р№[3]):", i + 1);
 			rewind(stdin);
 		}
 	}
@@ -78,7 +78,7 @@ void check_choice_task(int* ch)
 {
 	while (scanf_s("%d", ch) != 1 || *ch != 1 && *ch != 2 && *ch != 3 && *ch !=4 && *ch!=5|| getchar() != '\n')
 	{
-		printf("Ошибка.Попробуй снова\nВыберите задание:");
+		printf("РћС€РёР±РєР°.РџРѕРїСЂРѕР±СѓР№ СЃРЅРѕРІР°\nР’С‹Р±РµСЂРёС‚Рµ Р·Р°РґР°РЅРёРµ:");
 		rewind(stdin);
 	}
 }
@@ -86,27 +86,26 @@ void check_go_choice(int* ch)
 {
 	while (scanf_s("%d", ch) != 1 || *ch != 1 && *ch != 2 || getchar() != '\n')
 	{
-		printf("Ошибка.Попробуй снова\nОтвет:");
+		printf("РћС€РёР±РєР°.РџРѕРїСЂРѕР±СѓР№ СЃРЅРѕРІР°\nРћС‚РІРµС‚:");
 		rewind(stdin);
 	}
 }
 void show_cat(const struct bicycle* mas,const int* n)
 {
-	printf("Каталог:\n\nКласс:\t\t\tГод:\t\tЦвет:\n");
-	const static char* ForWhat[] = { "Горный", "Трюковой", "Городской", "Гибридный" }; //"Горный", "Трюковой", "Городской", "Гибридный"
+	printf("РљР°С‚Р°Р»РѕРі:\n\nРљР»Р°СЃСЃ:\t\t\tР“РѕРґ:\t\tР¦РІРµС‚:\n");
+	const static char* ForWhat[] = { "Р“РѕСЂРЅС‹Р№", "РўСЂСЋРєРѕРІРѕР№", "Р“РѕСЂРѕРґСЃРєРѕР№", "Р“РёР±СЂРёРґРЅС‹Р№" }; //"Р“РѕСЂРЅС‹Р№", "РўСЂСЋРєРѕРІРѕР№", "Р“РѕСЂРѕРґСЃРєРѕР№", "Р“РёР±СЂРёРґРЅС‹Р№"
 	for (int i = 0; i < *n; i++)
 	{
-		//printf("%d. %s\t\t%d\t\t",i+1, ForWhat[mas[i].class], mas[i].year);
 		printf("%d. ", i + 1);
-		fputs(ForWhat[mas[i].class], stdout);
+		printf("%s", ForWhat[mas[i].class]);
 		printf("\t\t%d\t\t", mas[i].year);
-		fputs(mas[i].colour, stdout);
+		printf("%s", mas[i].colour);
 		printf("\n");
 	}
 }
 void del_struct(struct bicycle* mas, int* n)
 {
-	printf("Какой велосипед вы хотите удалить?\n Ваш выбор(1-%d)", *n);
+	printf("РљР°РєРѕР№ РІРµР»РѕСЃРёРїРµРґ РІС‹ С…РѕС‚РёС‚Рµ СѓРґР°Р»РёС‚СЊ?\n Р’Р°С€ РІС‹Р±РѕСЂ(1-%d)", *n);
 	int x = 0;
 	check_del_str(&x, *n);
 	del(&mas, x, n);
@@ -117,7 +116,7 @@ void check_del_str(int* x, int n)
 {
 	while ((scanf_s("%d", x)) != 1 || *x < 1 || *x>n || getchar() != '\n')
 	{
-		printf("Ошибка. Попробуйте снова\nВаш выбор(1-%d)", n);
+		printf("РћС€РёР±РєР°. РџРѕРїСЂРѕР±СѓР№С‚Рµ СЃРЅРѕРІР°\nР’Р°С€ РІС‹Р±РѕСЂ(1-%d)", n);
 		rewind(stdin);
 	}
 }
@@ -133,11 +132,10 @@ void del(struct bicycle** mas, int x, int* n)
 	{
 		*mas = store;
 	}
-	free(*mas);
 }
 void sort_1_field(struct bicycle* mas, int* n)
 {
-	printf("По какому параметру вы хотите отсортировать каталог (Класс[1], Год[2], Цвет[3])?\nВаш выбор:");
+	printf("РџРѕ РєР°РєРѕРјСѓ РїР°СЂР°РјРµС‚СЂСѓ РІС‹ С…РѕС‚РёС‚Рµ РѕС‚СЃРѕСЂС‚РёСЂРѕРІР°С‚СЊ РєР°С‚Р°Р»РѕРі (РљР»Р°СЃСЃ[1], Р“РѕРґ[2], Р¦РІРµС‚[3])?\nР’Р°С€ РІС‹Р±РѕСЂ:");
 	int ch_1 = 0;
 	check_choice_sort(&ch_1);
     sort_1(mas, *n, ch_1);
@@ -147,7 +145,7 @@ void check_choice_sort(int* ch)
 {
 	while ((scanf_s("%d", ch)) != 1 || *ch != 1 && *ch!=2 && *ch!=3 || getchar() != '\n')
 	{
-		printf("Ошибка. Попробуйте снова\nВаш выбор");
+		printf("РћС€РёР±РєР°. РџРѕРїСЂРѕР±СѓР№С‚Рµ СЃРЅРѕРІР°\nР’Р°С€ РІС‹Р±РѕСЂ");
 		rewind(stdin);
 	}
 }
@@ -213,14 +211,13 @@ int int_check(int a, int b)
 }
 void sort_2_field(struct bicycle* mas, int* n)
 {
-	printf("По каким двум параметрам вы хотите отсортировать каталог (Класс[1], Год[2], Цвет[3])?\n\nПервый параметр:");
+	printf("РџРѕ РєР°РєРёРј РґРІСѓРј РїР°СЂР°РјРµС‚СЂР°Рј РІС‹ С…РѕС‚РёС‚Рµ РѕС‚СЃРѕСЂС‚РёСЂРѕРІР°С‚СЊ РєР°С‚Р°Р»РѕРі (РљР»Р°СЃСЃ[1], Р“РѕРґ[2], Р¦РІРµС‚[3])?\n\nРџРµСЂРІС‹Р№ РїР°СЂР°РјРµС‚СЂ:");
 	int ch_1 = 0;
 	int	ch_2 = 0;
 	check_choice_sort(&ch_1);
-	printf("Второй параметр:");
+	printf("Р’С‚РѕСЂРѕР№ РїР°СЂР°РјРµС‚СЂ:");
 	check_2_choices_sort(ch_1, &ch_2);
 	sort_2(mas, *n, ch_1, ch_2);
-
 	system("cls");
 	show_cat(mas,n);
 }
@@ -229,8 +226,8 @@ void check_2_choices_sort(int const ch_1, int* ch_2)
 	while ((scanf_s("%d", ch_2)) != 1 || getchar() != '\n' || *ch_2 < 1 || *ch_2 > 3 || ch_1 == *ch_2)
 	{
 		if (ch_1 == *ch_2)
-			printf("Два раза по одному параметру нельзя сортировать\n");
-		printf("Второй параметр:");
+			printf("Р”РІР° СЂР°Р·Р° РїРѕ РѕРґРЅРѕРјСѓ РїР°СЂР°РјРµС‚СЂСѓ РЅРµР»СЊР·СЏ СЃРѕСЂС‚РёСЂРѕРІР°С‚СЊ\n");
+		printf("Р’С‚РѕСЂРѕР№ РїР°СЂР°РјРµС‚СЂ:");
 		rewind(stdin);
 	}
 }
